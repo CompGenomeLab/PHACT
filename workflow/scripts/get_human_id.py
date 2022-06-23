03db20090769942a7c81323aab9a652c2771eb64
Emrah Akkoyun <emrah@levrek1.ib>
2022-06-23 17:41:31 +0300

    header updated

--- workflow/scripts/get_human_id.py
+++ workflow/scripts/get_human_id.py
@@ -5,6 +5,7 @@ def get_human_id(fasta_file):
     with open(str(fasta_file),'r') as f:
         for line in f:
             if line.startswith('>'):
+                line = line[0] + line[1:].replace('>', '-') 
                 header = line.split(">")[1].strip()
                 human_id = header.split(" ")[0]+"_"+header.split("OX=")[1].split(" ")[0]
                 human_id = re.sub(r'[^\w>]', '_',human_id)
